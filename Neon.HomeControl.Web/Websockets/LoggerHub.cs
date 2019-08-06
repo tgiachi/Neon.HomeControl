a552bd235c531075f6449d4e83be5a49a6f32bbc
tommaso giachi <squid@stormwind.it>
2019-08-06 19:12:15 +0200

    Added WebScoketManager library for handle websocket connections Added EventHub (/ws/events) for receive events from websocket Added LoggerHub for receive logger events

---
+++ Neon.HomeControl.Web/Websockets/LoggerHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Neon.HomeControl.Api.Core.Data.Logger;
+using Neon.HomeControl.Api.Core.Utils;
+using WebSocketManager;
+using WebSocketManager.Common;
+
+namespace Neon.HomeControl.Web.Websockets
+{
+	public class LoggerHub : WebSocketHandler
+	{
+		public LoggerHub(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+		{
+			AppUtils.LoggerEntries.CollectionChanged +=async (sender, args) =>
+			{
+				foreach (var t in args.NewItems)
+				{
+					var logEntry = t as LoggerEntry;
+					await SendMessageToAllAsync(new Message()
+					{
+						MessageType = MessageType.Text,
+						Data = logEntry.ToJson()
+					});
+				}
+			};
+		}
+	}
+}
